d459e19347309e07e034bd048a10aa9d6155de47
Isidro Goncalves <imisc@outlook.ie>
2017-04-09 20:03:45 +0100

    first commit, basic query to check connection to localdb
    more to follow

---
+++ dapperconsole/dapperconsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using System.Collections;
+
+namespace dapperconsole
+{
+    class Program
+    {
+        static string connection = ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+        static void Main(string[] args)
+        {
+            Console.Title = "testing Dapper";          
+            myquerydapper();
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// basic query to get all the contacts
+        /// </summary>
+        static void myquerydapper()
+        {
+           
+            IDbConnection db = new SqlConnection(connection);
+            var contacts = db.Query<Contact>("SELECT * FROM Contacts");
+
+            foreach (var x in contacts)
+                Console.WriteLine("{0}" +" " + "{1} ", x.FirstName,x.LastName );
+        }
+
+        
+    }
+}
