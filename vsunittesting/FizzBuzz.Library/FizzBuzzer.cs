9e5f2bf1064629a7776cf361be1199edc477c5e8
Isidro Goncalves <imisc@outlook.ie>
2017-04-18 18:14:19 +0100

    tdd basics from jeremy clark
    you never know who might need it

---
+++ vsunittesting/FizzBuzz.Library/FizzBuzzer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace FizzBuzz.Library
+{
+    public class FizzBuzzer
+    {
+        public static string GetValue(int input)
+        {
+            #region "after refactoring"
+            string output = string.Empty;
+            if (input % 3 == 0)
+                output += "Fizz";
+            if (input % 5 == 0)
+                output += "Buzz";
+            if (string.IsNullOrEmpty(output))
+                output = input.ToString();
+            return output;
+            #endregion
+
+            #region "before refactoring"
+            //if (input % 3 == 0 && input % 5 == 0)
+            //    return "FizzBuzz";
+            //if (input % 3 == 0)
+            //    return "Fizz";
+            //if (input % 5 == 0)
+            //    return "Buzz";
+            //return input.ToString();
+            #endregion
+        }
+    }
+}
---
+++ vsunittesting/FizzBuzz.Livrary.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FizzBuzz.Livrary.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FizzBuzz.Livrary.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("935644f0-6690-498d-b163-921bd002c55d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
