8c0dff5951b09c39a820ce1aa69b8e422b3bf410
Isidro Goncalves <imisc@outlook.ie>
2017-04-19 01:40:26 +0100

    sorting algorithms
    from the good people of w3resource

---
+++ sorting/sorting/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------sort shell-------------");
+            SortShell.ExecuteSortShell();
+            Console.WriteLine("-----------bogosort sort-------------");
+            BogoSort.ExecuteBogoSort();
+            Console.WriteLine("-----------bubble sort-------------");
+            BubbleSort.ExecuteBubbleSort();
+            Console.WriteLine("-----------counting sort-------------");
+            CountingSort.ExecuteCountingSort();
+            Console.WriteLine("-----------heap sort-------------");
+            HeapSort.ExecuteHeapSort();
+            Console.WriteLine("-----------insertion sort-------------");
+            Insertion.ExecuteInsertionSort();
+            Console.WriteLine("-----------merge sort-------------");
+            MergeSort.ExecuteMergeSort();
+            Console.WriteLine("-----------Permutation-------------");
+            Permutation.ExecutePermutationSort();
+            Console.WriteLine("-----------Quick Sort-------------");
+            QuickSort.ExecuteQuickSort();
+            Console.WriteLine("-----------Radix Sort-------------");
+            RadixSort.ExecuteRadixSort();
+            Console.WriteLine("-----------Selection Sort-------------");
+            SelectionSort.ExecuteSelectionSort();
+            Console.ReadLine();            
+        }
+    }
+}
